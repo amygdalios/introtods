be446cdfc05bc50c9a999111943c8d80b76cdcd6
Alex Amygdalios <amygdalios@icloud.com>
2024-12-05 16:56:48 +0200

    Fixed bugs and wrote comments to specify what each error was.

--- intro.c
+++ intro.c
@@ -9,13 +9,13 @@ int main() {
     scanf("%d", &num1);
     
     printf("Enter second number: ");
-    scanf("%d", &num2)
+    scanf("%d", &num2); // Missing semicolon
 
     // Calculating summary of numbers
-    sum = num1 * num2;
+    sum = num1 + num2; // Use of wrong operator
 
     // Printing the result
-    printf("The sum of %s and %d is %d\n", num1, num2, sum);
+    printf("The sum of %d and %d is %d\n", num1, num2, sum); // Printf expected a string, given variable is int.
 
-    return 0
+    return 0; // Missing semicolon
 }
